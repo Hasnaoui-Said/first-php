447ae45e2da293741920cd3b62a14dee66deeee0
hasnaoui-said <saidhasnaoui.uce@gmail.com>
2022-02-22 14:28:25 +0100

    add CRUD

--- fr/upload/upload.php
+++ fr/upload/upload.php
@@ -11,4 +11,19 @@
         move_uploaded_file($source, $destination);
     }
     
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Image</h1>
+    <img src="<?php echo $destination; ?>" alt="image">
+</body>
+</html>
